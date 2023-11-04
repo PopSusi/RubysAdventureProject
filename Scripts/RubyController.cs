7df5a65a9278b3c8f5d97ba7a26ae9e8d933f420
Susi <braeden8.susi6@gmail.com>
2023-11-04 01:24:13 -0400

    Health and Damage

--- Scripts/RubyController.cs
+++ Scripts/RubyController.cs
@@ -8,23 +8,53 @@ public class RubyController : MonoBehaviour
     public float walkSpeed;
     public float speed;
     public float runSpeed;
+    float horDir;
+    float verDir;
+    Rigidbody2D rb2d;
+
+    public int maxHealth;
+    public int health {get{return curHealth;}}
+    int curHealth;
+    
+    public float bufferTime = 2.0f;
+    bool isBuffer;
+    float bufferTimer;
     void Start()
     {
+        rb2d = GetComponent<Rigidbody2D>();
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 60;
         speed = walkSpeed;
+        curHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horDir = Input.GetAxis("Horizontal");
-        float verDir = Input.GetAxis("Vertical");
+        horDir = Input.GetAxis("Horizontal");
+        verDir = Input.GetAxis("Vertical");
 
-        Vector2 position = transform.position;
-
+        if(isBuffer){
+            bufferTimer -= Time.deltaTime;
+            if(bufferTimer < 0)
+                isBuffer = false;
+        }
+    }
+    void FixedUpdate(){
+        Vector2 position = rb2d.position;
         position.x = position.x + speed * horDir * Time.deltaTime;
         position.y = position.y + speed * verDir * Time.deltaTime;
-        transform.position = position;
+        rb2d.MovePosition(position);
+    }
+    public void ChangeHealth(int amnt){
+        if(amnt < 0){
+            if (isBuffer){
+                return;
+            }
+            curHealth += amnt;
+            isBuffer = true;
+            bufferTimer = bufferTime;
+        }
+        curHealth = Mathf.Clamp(curHealth + amnt, 0, maxHealth);
     }
 }
