626dcda5c40f1fd7e38d23831651c1c6c6b2174f
Susi <braeden8.susi6@gmail.com>
2023-11-04 00:19:20 -0400

    MoveAndEnvironment

--- Scripts/RubyController.cs
+++ Scripts/RubyController.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float walkSpeed;
+    public float speed;
+    public float runSpeed;
     void Start()
     {
-        
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+        speed = walkSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horDir = Input.GetAxis("Horizontal");
+        float verDir = Input.GetAxis("Vertical");
+
         Vector2 position = transform.position;
-        position.x = position.x + .1f;
+
+        position.x = position.x + speed * horDir * Time.deltaTime;
+        position.y = position.y + speed * verDir * Time.deltaTime;
         transform.position = position;
     }
 }
